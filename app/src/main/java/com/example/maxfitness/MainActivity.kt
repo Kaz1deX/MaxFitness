e9ea4afafa6543e5e2702474de082fe1c6d9d9cc
Kaz1deX <max2003255@gmail.com>
2023-07-15 16:21:12 +0300

    Add title and text

--- app/src/main/java/com/example/maxfitness/MainActivity.kt
+++ app/src/main/java/com/example/maxfitness/MainActivity.kt
@@ -3,41 +3,65 @@ package com.example.maxfitness
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.maxfitness.ui.theme.MaxFitnessTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaxFitnessTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Hello")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MaxFitnessTheme {
-        Greeting("Android")
+fun Greeting(str: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFAFD388))
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(start = 40.dp, top = 30.dp),
+            text = "MaxFitness",
+            fontSize = 35.sp,
+            color = Color(0xFF000000)
+            )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 40.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Number 1"
+            )
+            Text(
+                text = "Number 2"
+            )
+            Text(
+                text = "Number 3"
+            )
+            Text(
+                text = "Number 4"
+            )
+            Text(
+                text = "Number 5"
+            )
+        }
     }
 }
